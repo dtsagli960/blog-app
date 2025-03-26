2398f780a12dfe644bdb5d094d6d6617bd8aaa7c
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-26 02:09:49 +0000

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,13 +7,11 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
-app.use(express.json());
 app.use(routes);
 
 db
