f726df35cfacca8f3a893ebcd54efcf9a1c17b67
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-26 01:20:54 +0000

    Update api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/axiosApi",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
 });
 
 // Add JWT to requests
